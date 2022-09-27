84dbe31fb65bfdd9d172b00f0e5ac843abee28c6
Med-Wo <mravily@student.42.fr>
2022-09-27 18:11:48 +0200

    Add Routes :truck:
     - Change leaderboard route :truck:
     - Add default route for development :poop:

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LeaderboardComponent } from './features/leaderboard/components/leaderboard/leaderboard.component';
+import { NavBarComponent } from './features/leaderboard/components/nav-bar/nav-bar.component';
 import { SignInComponent } from './features/sign-in/sign-in.component';
 import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
+	{ path: '', component: NavBarComponent },
 	{ path: 'sign-in', component: SignInComponent },
-	{ path: 'leaderboard', component: LeaderboardComponent, canActivate: [AuthGuard] }];
+	{ path: 'view', loadChildren: () => import('./features/leaderboard/leaderboard.module').then(m => m.LeaderboardModule), canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
