dd23f3c12ca4010a210d2777a61e8a259d7c7e8f
Med-Wo <mravily@student.42.fr>
2022-09-27 12:44:44 +0200

    Add Feature :sparkles:
     - Add Sign-in components :sparkles:
     - Add path to sign-in in app-routing :truck:

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LeaderboardComponent } from './features/leaderboard/components/leaderboard/leaderboard.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
 import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
+	{ path: 'sign-in', component: SignInComponent },
 	{ path: 'leaderboard', component: LeaderboardComponent, canActivate: [AuthGuard] }];
 
 @NgModule({
