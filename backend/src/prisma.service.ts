39a82930f001778e65da761ea043d32c0e994314
Med-Wo <mravily@student.42.fr>
2022-09-27 12:21:48 +0200

    Fix prettier :lipstick:

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,21 +1,46 @@
-
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { getTopTen } from './prisma/leaderboard.service';
-import { setUser, getUserAccount, setFriend, set2FA, updateUserScore, updateUserStatus, getBlockedUsers, getFriends, getUser, uploadPhoto, getPhotoPath } from './prisma/user.service';
-import { getChannel, getChannelUsers, sendChannelMessage, setChannel, setChannelPass, setJoinChannel, setMakeAdmin, setMuteUser } from './prisma/channel.service';
-import { getMatchHistory, getNolostMatchs, getNoWinnedMatchs, getRatio, setMatch } from './prisma/match.service';
-
+import {
+  setUser,
+  getUserAccount,
+  setFriend,
+  set2FA,
+  updateUserScore,
+  updateUserStatus,
+  getBlockedUsers,
+  getFriends,
+  getUser,
+  uploadPhoto,
+  getPhotoPath,
+} from './prisma/user.service';
+import {
+  getChannel,
+  getChannelUsers,
+  sendChannelMessage,
+  setChannel,
+  setChannelPass,
+  setJoinChannel,
+  setMakeAdmin,
+  setMuteUser,
+} from './prisma/channel.service';
+import {
+  getMatchHistory,
+  getNolostMatchs,
+  getNoWinnedMatchs,
+  getRatio,
+  setMatch,
+} from './prisma/match.service';
 
 export interface accountUser {
-  score: number,
-  login: string,
-  name: string,
-  email: string,
-  photo: string,
-  online: boolean,
-  win: number,
-  lost: number,
+  score: number;
+  login: string;
+  name: string;
+  email: string;
+  photo: string;
+  online: boolean;
+  win: number;
+  lost: number;
 }
 
 export interface accountUser {
@@ -34,13 +59,13 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
-  
+
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
     });
   }
-  
+
   protected prisma = new PrismaClient();
 
   public getTopTen = getTopTen;
