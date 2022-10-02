88b19cef507f2574562ac7d3f1b1eaf0cba305dc
Med-Wo <mravily@student.42.fr>
2022-10-02 21:02:49 +0200

    Add New Features :sparkles:
     - Change statical to dynamical data :building_construction:
     - Add profile-button feature :sparkles:
     - Change routerLink in sidebar-menu file :truck:
     - Add sideBar models :wrench:
     - Create sideBar service :hammer:

--- frontend/src/app/core/components/sidebar-menu/components/sidebar-menu/sidebar-menu.component.ts
+++ frontend/src/app/core/components/sidebar-menu/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
-import { ShortListContact, ShortProfile } from '../../model/sidebar.model';
+import { SideBarData } from '../../model/sidebar.model';
 import { SidebarMenuService } from '../../services/sidebar-menu.service';
 
 @Component({
@@ -12,53 +12,49 @@ import { SidebarMenuService } from '../../services/sidebar-menu.service';
 
 export class SidebarMenuComponent implements OnInit {
 
+	// A utiliser pour la récupération des infos depuis la DB
+	// 	 data$!: SidebarData;
+	//   constructor(private sidebarServices:SidebarMenuService,
+	//  			 private cookieService: CookieService) { }
+	
+	//   ngOnInit(): void {
+	// 			this.data$ = this.sidebarServices.getAllUsers();
+	//   }
+		
+		isLogin() {
+			return this.cookieService.check('token');
+		}
+		
+	data$!: SideBarData;
 	constructor(private cookieService: CookieService) {}
-// A utiliser pour la r2cup2ration des infos depuis la DB
-//   contactList$!: Observable<ShortListContact[]>
-//   profile$!: ShortProfile;
-//   constructor(private sidebarServices:SidebarMenuService) { }
-
-//   ngOnInit(): void {
-// 		this.contactList$ = this.sidebarServices.getList();
-//  	this.profile$ = this.sidebarService.getProfile();
-// 		this.messages$ = this.sidebarService.getmessages();
-//   }
-
-	isLogin() {
-		return this.cookieService.check('token');
-	}
-	contactList!: ShortListContact[];
-	profile!: ShortProfile;
-	messages!: number;
 
 	ngOnInit(): void {
-		this.messages = 69;
-
-		this.profile = {
+		this.data$ = {
 			fullName: "Medhi Ravily",
-			avatar: 'https://cdn.intra.42.fr/users/mravily.jpg'
-		};
-
-		this.contactList = [
-			{
-				fullName: 'Juan Carlos Iglesias Gonzalez',
-				login: 'jiglesia',
-				isOnline: true,
-				avatar: 'https://cdn.intra.42.fr/users/jiglesia.jpg'
-			},
-			{
-				fullName: 'Augustin Desvallées',
-				login: 'adesvall',
-				isOnline: false,
-				avatar: 'https://cdn.intra.42.fr/users/adesvall.jpg'
-			},
-			{
-				fullName: 'Ulysse Peyret',
-				login: 'upeyret',
-				isOnline: false,
-				avatar: 'https://cdn.intra.42.fr/users/upeyret.jpg'
-			}
-		]
+			login: "mravily",
+			avatar: 'https://cdn.intra.42.fr/users/mravily.jpg',
+			friendsList: [
+				{
+					fullName: 'Juan Carlos Iglesias Gonzalez',
+					login: 'jiglesia',
+					isOnline: true,
+					avatar: 'https://cdn.intra.42.fr/users/jiglesia.jpg'
+				},
+				{
+					fullName: 'Augustin Desvallées',
+					login: 'adesvall',
+					isOnline: false,
+					avatar: 'https://cdn.intra.42.fr/users/adesvall.jpg'
+				},
+				{
+					fullName: 'Ulysse Peyret',
+					login: 'upeyret',
+					isOnline: false,
+					avatar: 'https://cdn.intra.42.fr/users/upeyret.jpg'
+				}
+			],
+			friends: 69,
+		}
 	}
-
+	
 }
