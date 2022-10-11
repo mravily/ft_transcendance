644fc14f15cf3ce20db23e1368bcf0df74add41f
jciglesias <jciglesias@student.42.fr>
2022-10-11 20:46:24 +0200

    profile update

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Session } from "@nestjs/common";
+import { Controller, Get, Param, Post, Req, Session } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 
 @Controller('profile')
@@ -21,5 +21,7 @@ export class ProfileController {
     }
 
     @Post('update')
-    updateProfile() {}
+    updateProfile(@Session() session: Record<string, any>, @Req() req: Request) {
+        console.log(req);
+    }
 }
