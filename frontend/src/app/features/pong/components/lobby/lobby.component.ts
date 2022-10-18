d727e47e0792ed852fc2c5fad5c6607408caef9e
Medhi RAVILY <mravily@e1r1p4.clusters.42paris.fr>
2022-10-18 14:44:08 +0200

    Fix Compile Errors :ambulance: LTS

--- frontend/src/app/features/pong/components/lobby/lobby.component.ts
+++ frontend/src/app/features/pong/components/lobby/lobby.component.ts
@@ -7,75 +7,77 @@ import { ChatService } from '../../../chat/services/chat.service';
 import { PongService } from '../../services/pong.service';
 
 @Component({
-	selector: 'app-lobby',
-	templateUrl: './lobby.component.html',
-	styleUrls: ['./lobby.component.scss']
+  selector: 'app-lobby',
+  templateUrl: './lobby.component.html',
+  styleUrls: ['./lobby.component.scss']
 })
 export class LobbyComponent implements OnInit, OnDestroy {
-	searchUser!: string;
+  searchUser!: string;
 
 
-	@ViewChild('findMatch')
-	findMatchButton!: ElementRef<HTMLButtonElement>;
-	@ViewChild('findPUMatch')
-	findPUMatchButton!: ElementRef<HTMLButtonElement>;
+  @ViewChild('findMatch')
+  findMatchButton!: ElementRef<HTMLButtonElement>;
+  @ViewChild('findPUMatch')
+  findPUMatchButton!: ElementRef<HTMLButtonElement>;
 
-	buttonText: string = "Find Opponent";
-	PUbuttonText: string = "Find Opponent";
-	// friends: any[];
-	searchForm!: FormGroup;
-	searchResult$!: Observable<IAccount[]>;
-	searchSubcription!: Subscription;
+  buttonText: string = "Find Opponent";
+  PUbuttonText: string = "Find Opponent";
+  // friends: any[];
+  searchForm!: FormGroup;
+  searchResult$!: Observable<IAccount[]>;
+  searchSubcription!: Subscription;
+  subs: Subscription[] = [];
 
-	constructor(private router: Router,
-		private pongServ: PongService,
-		private chatServ: ChatService,
-		private formBuilder: FormBuilder) {
-	}
+  constructor(private router: Router,
+              private pongServ: PongService,
+              private chatServ: ChatService,
+              private formBuilder: FormBuilder) { 
+  }
 
-	ngOnInit(): void {
-		this.searchForm = this.formBuilder.group({
-			search: [null]
-		});
-		this.searchSubcription = this.searchForm.valueChanges.pipe(
-			map((form) => form.search)
-		).subscribe((search) => {
-			this.chatServ.searchUsers(search);
-		});
-		this.searchResult$ = this.chatServ.getSearchUsersObs();
-		this.chatServ.getMatchFoundObs().subscribe((gameId: number) => {
-			console.log('game found', gameId);
-			this.router.navigate(['play', gameId]);
-		});
-		this.pongServ.gameFoundEvent.subscribe((id: number) => {
-			this.router.navigateByUrl('/play/' + id);
-		});
-		this.chatServ.getInviteObs().subscribe((login: string) => {
-			// a refaire avec medhi
-			console.log('invite', login);
-
-			this.chatServ.acceptInvite(login);
-		});
-	}
-	ngOnDestroy() {
-		this.searchSubcription.unsubscribe();
-	}
-	onMatchmaking() {
-		this.findMatchButton.nativeElement.disabled = true;
-		this.buttonText = "Searching...";
-
-		this.pongServ.getNewMatchmaking();
-	}
-	onPUMatchmaking() {
-		this.findPUMatchButton.nativeElement.disabled = true;
-		this.PUbuttonText = "Searching...";
-
-		this.pongServ.getNewPUMatchmaking();
-		this.pongServ.gameFoundEvent.subscribe((id: number) => {
-			this.router.navigateByUrl('/play/' + id);
-		});
-	}
-	onInvite(user: IAccount, powerup: boolean) {
-		this.chatServ.inviteUser(user.login, powerup);
-	}
+  ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+        search: [null]
+    });
+    this.searchSubcription = this.searchForm.valueChanges.pipe(
+      map((form) => form.search)
+    ).subscribe((search) => {
+      this.chatServ.searchUsers(search);
+    });
+    this.searchResult$ = this.chatServ.getSearchUsersObs();
+    this.subs.push(this.chatServ.getMatchFoundObs().subscribe((gameId: number) => {
+      console.log('game found', gameId);
+      this.router.navigate(['play', gameId]);
+    }));
+    this.subs.push(this.pongServ.gameFoundEvent.subscribe((id: number) => {
+      this.router.navigateByUrl('/play/' + id);
+    }));
+    this.subs.push(this.chatServ.getInviteObs().subscribe((login: string) => {
+      // a refaire avec medhi
+      console.log('invite', login);
+  
+      this.chatServ.acceptInvite(login);
+    }));
+  }
+  ngOnDestroy() {
+    this.searchSubcription.unsubscribe();
+    this.subs.forEach((sub) => sub.unsubscribe());
+  }
+  onMatchmaking() {
+    this.findMatchButton.nativeElement.disabled = true;
+    this.buttonText = "Searching...";
+    
+    this.pongServ.getNewMatchmaking();
+  }
+  onPUMatchmaking() {
+    this.findPUMatchButton.nativeElement.disabled = true;
+    this.PUbuttonText = "Searching...";
+    
+    this.pongServ.getNewPUMatchmaking();
+    this.pongServ.gameFoundEvent.subscribe((id: number) => {
+      this.router.navigateByUrl('/play/' + id);
+    });
+  }
+  onInvite(user: IAccount, powerup: boolean) {
+    this.chatServ.inviteUser(user.login, powerup);
+  }
 }
